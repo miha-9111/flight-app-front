1f9e5fcf2ce7cb279026ee010c70bb0fb7964aae
miha-9111 <miha-9111@yandex.ru>
2022-02-18 02:38:32 +0300

    Added -> shared.module + components/search-form component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchFormComponent } from '../components/search-form/search-form.component';
+
+
+@NgModule({
+  declarations: [
+    SearchFormComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SearchFormComponent
+  ]
+})
+export class SharedModule { }
